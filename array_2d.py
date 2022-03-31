bc3b3ca9e6c61df5e2ce77c7d04a962f2aa92174
rosemolthomasparava7 <77224849+rosemolthomasparava7@users.noreply.github.com>
2022-03-31 12:36:57 +0530

    Add files via upload

---
+++ array_2d.py
@@ -0,0 +1,18 @@
+import numpy as np
+array_2d = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+
+print(array_2d)
+
+
+print('Display the elements of 1st and 2nd column in 2nd and 3rd row')
+print(array_2d[1:3,0:2])
+
+print('Display 2nd and 3rd element of 1st row')
+print(array_2d[0,1:3])
+
+
+
+
+
+
+
